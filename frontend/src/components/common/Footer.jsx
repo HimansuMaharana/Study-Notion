6d2c20dbbc32bad94dfc28f6eb75d6efb2b0c9de
Himansu Maharana <153665170+HimansuMaharana@users.noreply.github.com>
2025-05-13 07:10:49 +0530

    Update Footer.jsx

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -174,21 +174,21 @@ const Footer = () => {
 
           <div className="text-center flex flex-col sm:flex-row ">
             <div className="flex ">
-              <span> Made with ❤️</span>
+              <span> Made by </span>
 
-              <Link to='https://github.com/Aniruddha-Gade' target="__blank" className="text-white hover:underline mr-1"
+              <Link to='https://github.com/HimansuMaharana' target="__blank" className="text-white hover:underline mr-1"
               >
-                Aniruddha Gade
+                Himansu Maharana & Ashlesha Patnaik
               </Link>
             </div>
-            <span> © 2023 Studynotion</span>
+            <span> © 2025 Studynotion</span>
           </div>
 
           <div className="flex items-center">
-            <a href="https://www.linkedin.com/in/aniruddha-gade-a48800231/" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/himansumaharana" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImLinkedin2 size={17} />
             </a>
-            <a href="https://www.github.com/Aniruddha-Gade" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.github.com/HimansuMaharana" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImGithub size={17} />
             </a>
           </div>
